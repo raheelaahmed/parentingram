84c9984e90655ada7a766bbec282a2058c3ccf8b
Raheela Aftab <raheela.aftab@gmail.com>
2024-10-10 23:09:02 +0000

    add model Comment and make migrations

--- blog/models.py
+++ blog/models.py
@@ -1,16 +1,30 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.utils import timezone
 
-
+# post model
 STATUS = ((0, "Draft"), (1, "Published"))
-# Create your models here.
+
+
 class Post(models.Model):
     title = models.CharField(max_length=200, unique=True)
     slug = models.SlugField(max_length=200, unique=True)
     author = models.ForeignKey(
-    User, on_delete=models.CASCADE, related_name="blog_posts"
-)
-content = models.TextField()
-created_on = models.DateTimeField(auto_now_add=True)
-status = models.IntegerField(choices=STATUS, default=0)
+        User, on_delete=models.CASCADE, related_name="blog_posts"
+    )
+    content = models.TextField(default="text")
+    created_on = models.DateTimeField(default=timezone.now)
+    status = models.IntegerField(choices=STATUS, default=0)
+    excerpt = models.TextField(blank=True)
+    updated_on = models.DateTimeField(default=timezone.now)
 
+    #comment model
+
+class Comment(models.Model):
+    post = models.ForeignKey(
+        Post, on_delete=models.CASCADE, related_name="comments")
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="commenter")
+    body = models.TextField()
+    approved = models.BooleanField(default=False)
+    created_on = models.DateTimeField(auto_now_add=True)
