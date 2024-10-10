391c9cbef7ccc53c1cf928fa10e8e1e9538c012f
Raheela Aftab <raheela.aftab@gmail.com>
2024-10-10 22:22:09 +0000

    add model post and migrate

--- parentingram/settings.py
+++ parentingram/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ssj4t$)pnpqn0f^@cr07v2bbwaoy_nozkat@$&eb@y5ov#+7$7'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
@@ -88,6 +88,11 @@ WSGI_APPLICATION = 'parentingram.wsgi.application'
 DATABASES = {
     'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
+
+CSRF_TRUSTED_ORIGINS = [
+    "https://*.codeinstitute-ide.net/",
+    "https://*.herokuapp.com"
+]
 
 
 # Password validation
