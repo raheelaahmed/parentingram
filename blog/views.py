b6845b53671fc9e8992369601d0891a8e9322b2f
Raheela Aftab <raheela.aftab@gmail.com>
2024-10-12 20:59:17 +0000

    add pagination in index file

--- blog/views.py
+++ blog/views.py
@@ -2,12 +2,13 @@ from django.shortcuts import render, get_object_or_404
 from django.views import generic
 from .models import Post
 
-
 # Create your views here.
+
+
 class PostList(generic.ListView):
-      queryset = Post.objects.filter(status=1)
-      template_name = "blog/index.html"
-paginate_by = 3
+    queryset = Post.objects.filter(status=1)
+    template_name = "blog/index.html"
+    paginate_by = 6
 
 
 def post_detail(request, slug):
@@ -23,12 +24,6 @@ def post_detail(request, slug):
 
     :template:`blog/post_detail.html`
     """
-
     queryset = Post.objects.filter(status=1)
     post = get_object_or_404(queryset, slug=slug)
-
-    return render(
-        request,
-        "blog/post_detail.html",
-        {"post": post},
-    )
+    return render(request, "blog/post_detail.html", {"post": post},)
