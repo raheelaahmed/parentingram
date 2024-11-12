874be32075e13778f382051d055874867d1796db
Raheela Aftab <raheela.aftab@gmail.com>
2024-11-12 23:57:20 +0000

    add summernote in contact us form

--- contact_us/forms.py
+++ contact_us/forms.py
@@ -1,8 +1,12 @@
 from django.forms import ModelForm
 from .models import Contact
+from django_summernote.widgets import SummernoteWidget
 
 
 class ContactForm(ModelForm):
     class Meta:
         model = Contact
         fields = ['email', 'subject', 'message']
+        widgets = {
+            'message': SummernoteWidget(),
+        }
