4b0cf7d4967aef619d75a02d7e039a8a01664df3
Raheela Aftab <raheela.aftab@gmail.com>
2024-11-19 23:53:48 +0000

    add removed user app

--- user/urls.py
+++
@@ -1,8 +0,0 @@
-from . import views
-from django.urls import path
-
-urlpatterns = [
-    path('', views.profile, name='profile'),
-    path('edit_profile<int:pk>/', views.EditProfileView.as_view(), name='edit_profile'),
-    path('delete_profile/', views.delete_profile, name='delete_profile')
-]
