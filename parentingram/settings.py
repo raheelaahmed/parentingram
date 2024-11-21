e43bc6cc104234884a860c37bbbcf7b4f99a1a85
Raheela Aftab <raheela.aftab@gmail.com>
2024-11-21 10:52:07 +0000

    add slug field hidden from user using css

--- parentingram/settings.py
+++ parentingram/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['8000-raheelaahme-parentingra-66vy4yoaie7.ws.codeinstitute-ide.net','.herokuapp.com']
 
